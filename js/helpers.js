7e8ed03d6268406f3df4e9889ecb71fe967f9bc0
tiago espanha stone <tiago.espanha@stone.com.br>
2025-12-07 16:33:59 -0300

    new pages

--- js/helpers.js
+++ js/helpers.js
@@ -5,20 +5,27 @@ const pb = new PocketBase('http://127.0.0.1:8090');
 const defaultLoadCMSDataOnHtml = (records) => {
   const contentDiv = document.getElementById('content');
     
-    contentDiv.innerHTML = '';
-  
-    records.forEach(record => {
-      const title = record.title ? `<h1>${record.title}</h1>` : undefined;
-      const memberHTML = `${title || ''} <p>${record.text}</p>`;
-      contentDiv.innerHTML += memberHTML;
-    });
+  contentDiv.innerHTML = '';
+
+  records.forEach(record => {
+    const title = record.title ? `<h1>${record.title}</h1>` : undefined;
+    const memberHTML = `${title || ''} <p>${record.text}</p>`;
+    contentDiv.innerHTML += memberHTML;
+  });
 
 }
 
 
-export async function loadCMSData(pbfilter = {type: "", filter: "", sort: "created"}, loadCMSDataOnHtml = defaultLoadCMSDataOnHtml) {
+export async function loadCMSData(pbFilters = {type: "", filter: "", sort: "created"}, loadCMSDataOnHtml = defaultLoadCMSDataOnHtml) {
   try {
-    const records = await pb.collection(pbfilter.type).getFullList();
+    let params = {}
+
+    if (pbFilters) {
+      params = {...pbFilters}
+    }
+
+    params.filter = undefined
+    const records = await pb.collection(pbFilters.type).getFullList(params);
 
     loadCMSDataOnHtml(records);
     
